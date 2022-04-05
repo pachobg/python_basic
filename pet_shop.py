b7bf421743a0d2c38bd6640e5de210bdde9ff468
Пламен Ангелов <103038521+pachobg@users.noreply.github.com>
2022-04-05 20:53:58 +0300

    Add files via upload

---
+++ pet_shop.py
@@ -0,0 +1,9 @@
+dog_food_package = int(input())
+cat_food_package = int(input())
+
+total_dog_food_price = dog_food_package * 2.5
+total_cat_food_price = cat_food_package * 4
+
+total_sum = total_dog_food_price + total_cat_food_price
+
+print(f"{total_sum} lv.")
